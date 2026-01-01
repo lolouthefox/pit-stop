ef5cd2203d6c409657bea5f8dfd4121b1be5e300
Liam Cheneval <liam.cheneval@gmail.com>
2026-01-01 16:59:26 +0100

    Fixed some paths missing the new .env admin password

--- src/routes/admin/api/menu/+server.ts
+++ src/routes/admin/api/menu/+server.ts
@@ -1,12 +1,12 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import { json } from '@sveltejs/kit';
-import { HARDCODED_PASSWORD } from '$lib/server/admin';
 import { db } from '$lib/server/db';
 import { menuItems } from '$lib/server/db/schema';
+import { ADMIN_PASSWORD } from "$env/static/private";
 
 export const GET: RequestHandler = async ({ cookies }) => {
     const passwordCookie = cookies.get('password');
-    if (passwordCookie !== HARDCODED_PASSWORD) {
+    if (passwordCookie !== ADMIN_PASSWORD) {
         cookies.delete('password', { path: '/' });
         return json({ error: 'Unauthorized' }, { status: 401 });
     }
