582df128b19f47f1d88e73ff56f5b990854b9c10
Liam Cheneval <liam.cheneval@gmail.com>
2026-07-07 20:06:49 +0200

    Fixed logout

--- src/routes/logout/+page.server.ts
+++ src/routes/logout/+page.server.ts
@@ -4,5 +4,5 @@ export async function load({ cookies }) {
 	cookies.delete('access_token', { path: '/' });
 	cookies.delete('refresh_token', { path: '/' });
 	cookies.delete('expires_at', { path: '/' });
-	redirect(307, '/');
+	redirect(307, '/onboarding');
 }
